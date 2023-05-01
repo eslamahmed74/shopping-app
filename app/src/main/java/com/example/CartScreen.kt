befe51556f5a98caa7b2e64c599f0ae39229b3dc
eslamahemed <proislamahmed74@gmail.com>
2023-05-01 16:06:06 +0300

    allmost finish home screen

--- app/src/main/java/com/example/CartScreen.kt
+++ app/src/main/java/com/example/CartScreen.kt
@@ -1,12 +1,54 @@
 package com.example
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.shoppingapp.R
 import com.example.shoppingapp.Screen
 
 @Preview(showSystemUi = true)
 @Composable
-fun CartScreen(){
-    Text(text = "CartScreen")
+fun CartScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Column {
+            Row(
+                modifier = Modifier.fillMaxWidth().padding(20.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(text = "My Cart", fontSize = 20.sp, color = colorResource(id = R.color.blue))
+                Text(
+                    text = "Total 3 Items",
+                    fontSize = 20.sp,
+                    color = colorResource(id = R.color.blue)
+                )
+            }
+
+            Box(modifier = Modifier
+                .fillMaxWidth()
+                .height(1.dp)
+                .background(colorResource(id = R.color.blue))
+            )
+        }
+    }
+
 }
